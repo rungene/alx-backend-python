cfde538658318371ee3c72bd86e9ff576471c4f4
rungene <rungenesamuel@gmail.com>
2023-08-03 08:39:59 -0700

    Task: 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+
+"""
+2-floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    takes a float n as argument and returns the
+    floor of the float."""
+    return math.floor(n)
