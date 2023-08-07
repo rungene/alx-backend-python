8f38e11ba7f79c2bea05e125bf392d67d94ccc46
rungene <rungenesamuel@gmail.com>
2023-08-07 04:04:23 -0700

    type-annotations functions and coroutines

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,12 +6,12 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     asynchronous coroutine that returns generated random value
     """
     # random floating point generated
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     # Wait for specified amount of time asyncronusly
     await asyncio.sleep(delay)
     return delay
