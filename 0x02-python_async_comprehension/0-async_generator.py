f3405369ea1c85e9c2814f52d6a1f5a48332233c
rungene <rungenesamuel@gmail.com>
2023-08-08 18:40:10 -0700

    Task: 0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+0-async_generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Async generators that takes no arguments."""
+    count = 0
+    while count < 10:
+        await asyncio.sleep(random.uniform(0, 1))
+        yield random.uniform(0, 10)
+        count += 1
