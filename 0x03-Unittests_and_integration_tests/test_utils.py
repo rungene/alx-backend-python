8329be78017301587add630c1fff5012162d9ffc
rungene <rungenesamuel@gmail.com>
2023-08-29 02:26:19 +0000

    Task 1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,14 +10,27 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """Test suite for access_nested_map function"""
     @parameterized.expand([
-        (nested_map={"a": 1}, ("a",), 1),
-        (nested_map={"a": {"b": 2}}, ("a",), {"b": 2}),
-        (nested_map={"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """
-        This method test that the method return value
+        Assert This method test that the method return value
         is the expected result
         """
         result = access_nested_map(nested_map, path)
         self.assertEqual(expected_result, result)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_key):
+        """
+        This method Use the assertRaises context manager to
+        test that a KeyError is raised
+        """
+        with self.assertRaises(KeyError):
+            result = access_nested_map(nested_map, path)
