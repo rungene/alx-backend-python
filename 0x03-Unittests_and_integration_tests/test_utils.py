bd5a024025521f7d679fcf93027b58bca5340b45
rungene <rungenesamuel@gmail.com>
2023-09-02 02:56:38 +0000

    Corrections

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """
@@ -25,7 +25,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({"a": 1}, ("a", "b"), "b")
     ])
     def test_access_nested_map_exception(self, nested_map, path,
                                          expected_key):
@@ -41,7 +41,7 @@ class TestGetJson(unittest.TestCase):
     """Test suite for get_json function"""
     @parameterized.expand([
         ('http://example.com', {"payload": True}),
-        ('http://holberton.io', {"payload": False}),
+        ('http://holberton.io', {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
         """test that utils.get_json returns the expected result."""
@@ -50,6 +50,5 @@ class TestGetJson(unittest.TestCase):
 
         with patch('requests.get', return_value=mock_response):
             result = get_json(test_url)
-
+            self.assertEqual(result, test_payload)
             mocked_response.json.assert_called_once()
-            self.assertEqual(result.json(), test_payload)
